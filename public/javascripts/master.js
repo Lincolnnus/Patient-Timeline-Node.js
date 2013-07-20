25975a514516c7a27a4ba9b0b1275b39ed6784d6
Shao <shaohuan.li@gmail.com>
2013-07-20 16:59:05 +0800

    Added LocalScroll and ScrollTO

---
+++ public/javascripts/master.js
@@ -0,0 +1,27 @@
+(function () {
+  $('#nav').localScroll(800);
+  var $sidebar = $('.sidenav'),
+    top = $sidebar.offset().top,
+    $links = $sidebar.find('a');
+  $(window).on('scroll', function (e) {
+    var scrollY = this.scrollY;
+    if (scrollY > top){
+      $sidebar.addClass('fixed');
+    } else if (scrollY < top) {
+      $sidebar.removeClass('fixed');
+    }
+    $sidebar.find('.current').removeClass('current');
+    $links.each(function () {
+      var $this = $(this),
+        $section = $($this.attr('href'));
+      if ($section.offset().top < (scrollY+2)) {
+        $sidebar.find('.current').removeClass('current');
+        $this.addClass('current');
+      } else {
+        $this.removeClass('current');
+      }
+    });
+    $sidebar.find('.current').parents('li').addClass('current');
+  });
+
+}());
