7fecca89803c8319d2f77a10ba7bce5207b67be5
Shao <shaohuan.li@gmail.com>
2013-08-28 00:05:22 +0800

    Added timelineajax,labsajax and detailajax functions

--- public/js/bindaas.js
+++ public/js/bindaas.js
@@ -947,6 +947,49 @@ function showDetail(){
     new EJS({url: '../templates/problems.ejs'}).update('problems', {problems: problems});
     new EJS({url: '../templates/procedures.ejs'}).update('procedures', {procedures: procedures});
 }
+
+function showDetailAjax(){
+    getCCDA('/timeline/demographics.json',function(demographics){
+      new EJS({url: '../templates/demographics.ejs'}).update('demographics', {demographics: demographics});
+    });
+    getCCDA('/timeline/problem.json',function(problems){
+        new EJS({url: '../templates/problems.ejs'}).update('problems', {problems: problems});
+    });
+    getCCDA('/timeline/procedure.json',function(procedures){
+        new EJS({url: '../templates/procedures.ejs'}).update('procedures', {procedures: procedures});
+    });
+    getCCDA('/timeline/immunization.json',function(immunizations){
+        new EJS({url: '../templates/immunizations.ejs'}).update('immunizations', {immunizations: immunizations});
+    });
+    getCCDA('/timeline/medication.json',function(medications){
+        new EJS({url: '../templates/medications.ejs'}).update('medications', {medications: medications});
+    });
+    getCCDA('/timeline/encounter.json',function(encounters){
+        new EJS({url: '../templates/encounters.ejs'}).update('encounters', {encounters: encounters});
+    });
+}
+function showTimelineAjax(){
+    getCCDA('/timeline/demographics.json',function(demographics){
+      new EJS({url: '../templates/demographics.ejs'}).update('demographics', {demographics: demographics});
+    });
+    getCCDA('/timeline/allergy.json',function(allergies){
+        new EJS({url: '../templates/allergies.ejs'}).update('allergies', {allergies: allergies});
+    });
+}
+function showLabsAjax(){
+    getCCDA('/timeline/lab.json',function(labs){
+       new EJS({url: '../templates/demographics.ejs'}).update('demographics', {demographics: demographics});
+      // new EJS({url: '../templates/labs.ejs'}).update('labs', {labs: labs});
+    });
+}
+function getCCDA(url,callback){
+     $.ajax({
+        url: url,
+        contentType: "application/json",
+    }).done(function(data) {
+      callback(data);
+    });
+}
 function showTimeline(){
     new EJS({url: '../templates/demographics.ejs'}).update('demographics', {demographics: demographics});
     new EJS({url: '../templates/allergies.ejs'}).update('allergies', {allergies: allergies});
